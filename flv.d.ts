b45d3466cf21cd8df9df33436c721effb950f6be
xqq <xqq@xqq.im>
2017-06-21 14:28:24 +0900

    [Release]: v1.3.1: Support SourceBuffer auto cleanup to reduce memory usage for live stream

--- flv.d.ts
+++ flv.d.ts
@@ -54,6 +54,10 @@ declare namespace FlvJs {
         lazyLoadRecoverDuration?: number,
         deferLoadAfterSourceOpen?: boolean,
 
+        autoCleanupSourceBuffer?: boolean,
+        autoCleanupMaxBackwardDuration?: number,
+        autoCleanupMinBackwardDuration?: number,
+
         statisticsInfoReportInterval?: number,
 
         accurateSeek?: boolean,
@@ -89,7 +93,7 @@ declare namespace FlvJs {
         detachMediaElement(): void;
         load(): void;
         unload(): void;
-        play(): void;
+        play(): Promise<void>;
         pause(): void;
         type: string;
         buffered: TimeRanges;
